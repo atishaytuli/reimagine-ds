4ff2fcdc814d54d41bf945047846dfde8e70793f
atishaydesigning <atishay@designingsolutions.co.in>
2025-03-19 17:55:02 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </head>
       <body className={cn("min-h-screen bg-background", inter.className)}>
         <ThemeProvider
           attribute="class"
